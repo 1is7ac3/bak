1ff57efe91f2fd251dbf862b812242ed7a617624
1is7ac3 <isaac.qa13@gmail.com>
2025-05-04 15:36:49 -0400

    fish: add alias to nvidia prime

---
+++ config/fish/functions/prime.fish
@@ -0,0 +1,4 @@
+function prime --wraps='__NV_PRIME_RENDER_OFFLOAD=1 __VK_LAYER_NV_optimus=NVIDIA_only __GLX_VENDOR_LIBRARY_NAME=nvidia DRI_PRIME=1' --description 'alias prime=__NV_PRIME_RENDER_OFFLOAD=1 __VK_LAYER_NV_optimus=NVIDIA_only __GLX_VENDOR_LIBRARY_NAME=nvidia DRI_PRIME=1'
+  __NV_PRIME_RENDER_OFFLOAD=1 __VK_LAYER_NV_optimus=NVIDIA_only __GLX_VENDOR_LIBRARY_NAME=nvidia DRI_PRIME=1 $argv
+        
+end
